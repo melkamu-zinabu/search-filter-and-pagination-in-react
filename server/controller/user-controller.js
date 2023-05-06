c8c95b5f58d02c6112deab4b5cd4e6f437b9a774
melkamu <zmelkamu918@gmail.com>
2023-05-06 21:08:28 +0300

    the endpoint for search and filter done

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -48,3 +48,54 @@ export const signup=async(req,res,next)=>{
    return  res.status(201).json({users})
 
 }
+
+
+
+
+
+
+//paginated serach filter user component
+
+// Route: GET /api/users
+// Get paginated, filtered, and searched user data
+export const getfiltereduser= async (req, res) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const pageSize = parseInt(req.query.pageSize) || 10;
+    const search = req.query.search || "";
+    const filter = req.query.filter || "";
+
+    const skip = (page - 1) * pageSize;
+    const limit = pageSize;
+
+    // Build the query conditions based on the search and filter parameters
+    const conditions = {};
+    if (search) {
+      conditions.$or = [
+        { name: { $regex: search, $options: "i" } },
+        { email: { $regex: search, $options: "i" } },
+      ];
+    }
+    if (filter) {
+      conditions.role = filter;
+    }
+
+    // Get the total count of matching users
+    const totalCount = await user.countDocuments(conditions);
+
+    // Fetch the paginated user data
+    const users = await user.find(conditions)
+      .skip(skip)
+      .limit(limit);
+
+    res.json({
+      items: users,
+      totalItems: totalCount,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: "Failed to fetch users" });
+  }
+};
+
+
